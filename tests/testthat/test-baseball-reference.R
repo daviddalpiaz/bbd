82415b707e04427a664170406b06a223d524650e
David Dalpiaz <dalpiaz2@illinois.edu>
2022-08-11 13:14:43 -0500

    avoid http request when testing on CI

--- tests/testthat/test-baseball-reference.R
+++ tests/testthat/test-baseball-reference.R
@@ -1,18 +1,15 @@
-bref_bat = bref_war_daily_batter()
-bref_pit = bref_war_daily_pitcher()
-
 test_that("bref war functions always return a data frame", {
   skip_on_cran()
   skip_on_ci()
-  expect_true(is.data.frame(bref_bat))
-  expect_true(is.data.frame(bref_pit))
+  expect_true(is.data.frame(bref_war_daily_batter()))
+  expect_true(is.data.frame(bref_war_daily_pitcher()))
 })
 
 test_that("bref war functions return a non-empty data frame", {
   skip_on_cran()
   skip_on_ci()
-  expect_true(nonempty_df(bref_bat))
-  expect_true(nonempty_df(bref_pit))
+  expect_true(nonempty_df(bref_war_daily_batter()))
+  expect_true(nonempty_df(bref_war_daily_pitcher()))
 })
 
 # TODO: test for column names
