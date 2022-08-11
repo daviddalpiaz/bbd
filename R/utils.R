437f6807cca9f351ac48a59904b06e2580da64c9
David Dalpiaz <dalpiaz2@illinois.edu>
2022-08-10 20:51:09 -0500

    don't export utility functions

--- R/utils.R
+++ R/utils.R
@@ -3,11 +3,6 @@
 #' @param x An R object.
 #'
 #' @return A logical vector
-#' @export
-#'
-#' @examples
-#' nonempty_df(data.frame())
-#' nonempty_df(data.frame(1))
 nonempty_df = function(x) {
   stopifnot(is.data.frame(x))
   return(nrow(x) != 0)
@@ -18,11 +13,6 @@ nonempty_df = function(x) {
 #' @param x A vector
 #'
 #' @return A logical vector
-#' @export
-#'
-#' @examples
-#' all_na(c(1, NA, 3))
-#' all_na(c(NA, NA, NA, NA))
 all_na = function(x) {
   return(all(is.na(x)))
 }
