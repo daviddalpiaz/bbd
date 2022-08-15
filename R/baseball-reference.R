a739365471e01816a3158cf1e592e7dc344701e3
David Dalpiaz <dalpiaz2@illinois.edu>
2022-08-15 15:58:10 -0500

    add bref examples

--- R/baseball-reference.R
+++ R/baseball-reference.R
@@ -5,6 +5,11 @@
 #'
 #' @return A `data.frame` or `tibble` containing batter WAR data.
 #' @export
+#'
+#' @examples
+#' \dontrun{
+#' bref_war_daily_batter()
+#' }
 bref_war_daily_batter = function(tibble = TRUE) {
   url = "https://www.baseball-reference.com/data/war_daily_bat.txt"
   data = data.table::fread(
@@ -26,6 +31,11 @@ bref_war_daily_batter = function(tibble = TRUE) {
 #'
 #' @return A `data.frame` or `tibble` containing pitcher WAR data.
 #' @export
+#'
+#' @examples
+#' \dontrun{
+#' bref_war_daily_pitcher()
+#' }
 bref_war_daily_pitcher = function(tibble = TRUE) {
   url = "https://www.baseball-reference.com/data/war_daily_pitch.txt"
   data = data.table::fread(
