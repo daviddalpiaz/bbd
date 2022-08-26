517bd91b20575f7e78fffc16fda931d9506e63d9
David Dalpiaz <dalpiaz2@illinois.edu>
2022-08-25 20:08:13 -0500

    allow for return of unprocessed chadwick data

--- R/chadwick.R
+++ R/chadwick.R
@@ -1,34 +1,42 @@
-chadwick_people = function(tibble = TRUE) {
+# TODO: make a process function, add arg to main function
+chadwick_people = function(process = FALSE, tibble = TRUE) {
+
   url = "https://raw.githubusercontent.com/chadwickbureau/register/master/data/people.csv"
   data = data.table::fread(url, data.table = FALSE, verbose = FALSE, showProgress = FALSE)
-  vars = c(
-    "key_person",
-    "key_uuid",
-    "key_mlbam",
-    "key_retro",
-    "key_bbref",
-    "key_bbref_minors",
-    "key_fangraphs",
-    "key_npb",
-    "name_last",
-    "name_first",
-    "name_suffix"
-  )
-  data = data[, vars]
 
-  data = data[!is.na(data$key_mlbam), ]
+  if (process) {
 
-  names = paste(data$name_first, data$name_last)
-  names = paste0(names, ifelse(data$name_suffix == "", "", paste0(" ", data$name_suffix)))
-  data$name_first = NULL
-  data$name_last = NULL
-  data$name_suffix = NULL
+    vars = c(
+      "key_person",
+      "key_uuid",
+      "key_mlbam",
+      "key_retro",
+      "key_bbref",
+      "key_bbref_minors",
+      "key_fangraphs",
+      "key_npb",
+      "name_last",
+      "name_first",
+      "name_suffix"
+    )
 
-  data = data.frame(name = names, data)
+    data = data[, vars]
+    data = data[!is.na(data$key_mlbam), ]
+
+    names = paste(data$name_first, data$name_last)
+    names = paste0(names, ifelse(data$name_suffix == "", "", paste0(" ", data$name_suffix)))
+    data$name_first = NULL
+    data$name_last = NULL
+    data$name_suffix = NULL
+
+    data = data.frame(name = names, data)
+
+  }
 
   if (tibble) {
     return(tibble::as_tibble(data))
   }
 
   return(data)
+
 }
