6ab794650d4ac4bc5e412d0e5e916b5c9ba130f2
David Dalpiaz <dalpiaz2@illinois.edu>
2023-02-05 10:59:27 -0600

    less duplicated downloading

--- tests/testthat/test-baseball-reference.R
+++ tests/testthat/test-baseball-reference.R
@@ -1,11 +1,14 @@
+bref_bat = bref_war_daily_batter()
+bref_pit = bref_war_daily_pitcher()
+
 test_that("bref war functions always return a data frame", {
-  expect_true(is.data.frame(bref_war_daily_batter()))
-  expect_true(is.data.frame(bref_war_daily_pitcher()))
+  expect_true(is.data.frame(bref_bat))
+  expect_true(is.data.frame(bref_pit))
 })
 
 test_that("bref war functions return a non-empty data frame", {
-  expect_true(nonempty_df(bref_war_daily_batter()))
-  expect_true(nonempty_df(bref_war_daily_pitcher()))
+  expect_true(nonempty_df(bref_bat))
+  expect_true(nonempty_df(bref_pit))
 })
 
 # TODO: test for column names
