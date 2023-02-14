13d6cca2d5be5e840a1a1f05eaa5241b8914b338
David Dalpiaz <dalpiaz2@illinois.edu>
2023-02-13 20:45:41 -0600

    more data docs (fix check)

--- R/data.R
+++ R/data.R
@@ -3,9 +3,21 @@
 #' TODO: put description here
 #'
 #' @format ## `people`
-#' A data frame with 100,715 rows and 9 columns:
+#' A data frame with 100,965 rows and 9 columns:
 #' \describe{
 #'   \item{name}{name Player name}
 #'   ...
 #' }
 "people"
+
+#' Statcast People Information
+#'
+#' TODO: put description here
+#'
+#' @format ## `people_sc`
+#' A data frame with 100.965 rows and 2 columns:
+#' \describe{
+#'   \item{name}{name Player name}
+#'   ...
+#' }
+"people_sc"
