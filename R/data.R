ca3a5b282616444259a9c260b9f0b6e5b1a5122d
David Dalpiaz <dalpiaz2@illinois.edu>
2023-02-15 20:29:46 -0600

    better document people data

--- R/data.R
+++ R/data.R
@@ -1,11 +1,14 @@
-#' People Information
+#' People Information from the Chadwick Baseball Bureau Register
 #'
-#' TODO: put description here
+#' A dataset containing baseball player names and unique identifiers used by
+#' various sources. Original data source and documentation:
+#' [`chadwickbureau/register`](https://github.com/chadwickbureau/register)
 #'
-#' @format ## `people`
-#' A data frame with 100,965 rows and 9 columns:
+#' @format A data frame with `r nrow(people)` rows and `r ncol(people)` columns:
 #' \describe{
-#'   \item{name}{name Player name}
-#'   ...
+#'   \item{`name`}{`[character]` Player's full name listed as: Given Family Suffix}
+#'   \item{`key_mlbam`}{`[integer]` Player's MLB Advanced Media (MLBAM) identifer}
+#'   \item{`key_bbref`}{`[character]` Player's [Baseball-Reference](https://www.baseball-reference.com/) identifer}
+#'   \item{`key_fangraphs`}{`[integer]` Player's [FanGraphs](https://www.fangraphs.com/) identifer}
 #' }
 "people"
